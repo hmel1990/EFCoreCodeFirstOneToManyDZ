90c68e6fda839d349474214b3d8769588d149ec6
Hmele_sc73@student.itstep.org <Hmele_sc73@student.itstep.org>
2025-03-13 14:02:27 +0200

    Anotation

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace EFCoreCodeFirstOneToManyDZ.Models;
 
 public partial class Category
 {
+    [Key]
+
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Название обязательно для заполнения")]
+    [StringLength(100, ErrorMessage = "Название не может превышать 100 символов")]
     public string? Name { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
--- Models/Order.cs
+++ Models/Order.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCoreCodeFirstOneToManyDZ.Models;
 
 public partial class Order
 {
+    [Key]
     public int Id { get; set; }
 
     [ForeignKey("Product")]
     public int? IdProduct { get; set; }
 
+    [Range(0, 100, ErrorMessage = "количество должно быть в пределах от 0 до 1000")]
     public int? Quantity { get; set; }
 
     public virtual Product? IdProductNavigation { get; set; }
